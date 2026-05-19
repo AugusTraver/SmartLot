98c278e3799f4dd233d72cb493767e5ced4a2039
FRANCO URQUIZO BARBA <49257286@ort>
2026-05-19 10:31:49 -0300

    agreglado el navegador

--- src/componentes/header_admin.jsx
+++ src/componentes/header_admin.jsx
@@ -1,7 +1,7 @@
 import logo from "../Imagenes/Logo_smartlot.png";
 import "./header_admin.css";
 import { FaRegBell } from "react-icons/fa";
-import { RxAvatar } from "react-icons/rx";  
+import { RxAvatar } from "react-icons/rx";
 
 function Header() {
 
@@ -18,11 +18,14 @@ function Header() {
         </h1>
       </div>
       <div className="header-right">
-         <FaRegBell  size={30} color="black" />
-         <RxAvatar  size={40} color="black" />
-         
-       
-     </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30 " fill="currentColor">
+          <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="currentColor">
+          <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+        </svg>
+
+      </div>
     </div>
   );
 }
